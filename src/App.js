0f13e8412234696929654151d7453329d7edbf74
Carl Wilke <33393171+carlwilke@users.noreply.github.com>
2021-06-25 21:25:02 +0100

    New text update to test the pipeline

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}. This is running with React</div>;
+  const value = 'friends';
+  return <div>Hello {value}. This is running with React in an Azure static web page. Pretty cool ??</div>;
 }
 
 export default App;
